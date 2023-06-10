7b02894a3737368397d25bc3bbe4feafcb9fa216
Nersae ginopal <j.chavarria121121@hotmail.com>
2023-06-10 19:11:39 +0200

    script refactored, no need for the classes the way they were usen

---
+++ programrestructured.py
@@ -0,0 +1,41 @@
+import random
+
+ourSymbols = {1 : "$" , 2 : "Ace", 3 : "Pot", 4 : "Cereja", 5 : "K", 6 : "j", 7: "Poker"}
+
+
+#function to retrieve 3 random numbers from 1 to 7
+
+def rowscore():
+        scores = []
+        scores.append(random.randint(1, 7))
+        scores.append(random.randint(1, 7))
+        scores.append(random.randint(1, 7))
+        return scores
+
+#function to convert the numbers into our slot symbols
+
+def symbolconversion(ourlist):
+        play = []
+        for number in ourlist:
+           if number in ourSymbols:
+               play.append(ourSymbols[number])
+        return play
+
+
+play = rowscore()
+
+print(symbolconversion(play))
+
+slotrow = symbolconversion(play)
+
+def prize(play):
+    for symbol in play:
+        if play.count(symbol) == 3:
+            return "Jackpot"
+        elif play.count(symbol) > 1:
+            return "Two of a Kind"
+    else:
+        return "Better luck next time" 
+   
+
+print(prize(slotrow))
